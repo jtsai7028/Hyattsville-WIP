de355ef130edb9dfda3679101fbc5444aad0e545
Jessica Tsai <jtsai123@terpmail.umd.edu>
2020-11-18 17:08:28 -0500

    Connect the page

--- server.js
+++ server.js
@@ -3,13 +3,18 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('/'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('Pages'));
 
-app.route('/')
+app.route('/index')
   .get((req, res) => {
     console.log('GET request detected');
     res.send("working!!");
-  });
+  })
+  .post((req, res) => {
+    console.log('POST request detected');
+    console.log('Form data in req.body', req.body);});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
