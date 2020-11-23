2368640623c8aadba14fb3f505b45730f4129df5
Jessica Tsai <jtsai123@terpmail.umd.edu>
2020-11-23 18:25:06 -0500

    try out new get

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from "path";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,8 +9,9 @@ app.use(express.json());
 app.use(express.static('pages'));
 
 app.route('/index')
-  .get((req, res) => {
+  .get("*", (req, res) => {
     console.log('GET request detected');
+    res.sendFile(path.join(Hyattsville-WIP, "pages", "index.html"));
   });
 
 app.listen(port, () => {
