d20263954d6ee70fa527e2da07d96893e8fa0250
Jessica Tsai <jtsai123@terpmail.umd.edu>
2020-11-23 17:06:16 -0500

    trying out assign2 server

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
+// These are our required libraries to make the server work.
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
 import express from 'express';
+import dotenv from 'dotenv';
+import fetch from 'node-fetch'; //lab7
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,9 +14,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('pages'));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.route('/index')
-  .get((req, res) => {
+  .get(async (req, res) => {
     console.log('GET request detected');
+  })
+  .post((req, res) => {
+    console.log('POST request detected');
+    console.log('Form data in req.body', req.body);
   });
 
 app.listen(port, () => {
